571b339517cd6e3033bec46f428bb6f2b98f35fd
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-26 01:24:51 +0200

    accepted version

--- Program.cs
+++ Program.cs
@@ -2,9 +2,30 @@
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
-        
-        Console.WriteLine("1_Two:Start ");   
+        // int[] nums = {2,7,11,15};
+        // int target = 9;
+        int[] nums = {0,4,3,0};
+        int target = 0;
+        // int[] nums = {3,2,4};
+        // int target = 6;
+        // int[] nums = {3,3};
+        // int target = 6;
+        // int[] nums = {-1,4,-3,7};
+        // int target = -4;
+        var res = TwoSum(nums,target);
+        Console.WriteLine("1_Two:Start "+res);   
     }
-   
-    
+
+    public static int[] TwoSum(int[] nums, int target) {
+        int[] position = new int[2];
+        for(int i = 0; i < nums.Length; i++){
+           for(int j = i+1;j<nums.Length;j++ ){
+                if(nums[i]+nums[j] == target){
+                   position[0] = i;
+                   position[1] = j;
+                }
+            }   
+        }
+        return position;
+    }
 }
