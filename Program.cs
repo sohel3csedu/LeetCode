634b1733e11fe8f52a58e02b5cd90bfdccbe34aa
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-25 16:05:14 +0200

    second comment after fixing some more item

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ public class Program{
     public static void Main(string[] args){
         Program program = new Program();
         string[] testString = {"flower","flow","flight"};
+        //string[] testString = {"dog","racecar","car"};
         var sortedString = testString.OrderBy(a => a.Length).ToArray();
         string test = program.LongestCommonPrefix(sortedString);
         Console.WriteLine("longest substring :" +test);   
@@ -10,15 +11,19 @@ public class Program{
     public string LongestCommonPrefix(string[] strs) {
         int longestMatch = 0;
         bool lengthOver200 = strs.Where(a => a.Length > 200).Any();
-        
-        for(int i = 0; i < strs.Length; i++){
+        if(lengthOver200){
+            return string.Empty;
+        }else
+        {
+            for(int i = 0; i < strs.Length; i++){
             for(int j = i + 1; j < strs.Length; j++){
                 int currentMatch = GetLongestMatchNumberBetweenTwoString(strs[i],strs[i+1]);
                 longestMatch = currentMatch;         
             }
         }
         
-        return strs[0].Substring(0,longestMatch);
+        return strs[0].Substring(0,longestMatch);    
+        }
     }
     public int GetLongestMatchNumberBetweenTwoString(string s1, string s2){
         int longestMatchBetweenTheseTwo = 0;
@@ -27,6 +32,8 @@ public class Program{
             char c2 = s2[k];
             if(c1 == c2)
                 longestMatchBetweenTheseTwo++;
+            else
+                return longestMatchBetweenTheseTwo;
         }
         return longestMatchBetweenTheseTwo;
     }
