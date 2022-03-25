826ba65798fc525502b8f3daf7f3f36be29e150b
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-25 15:01:16 +0200

    first working version

--- Program.cs
+++ Program.cs
@@ -2,12 +2,32 @@
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
-        string[] testString = {"Array","Of","String"};
-        
-        string test = program.LongestCommonPrefix(testString);
-        Console.WriteLine(test);   
+        string[] testString = {"flower","flow","flight"};
+        var sortedString = testString.OrderBy(a => a.Length).ToArray();
+        string test = program.LongestCommonPrefix(sortedString);
+        Console.WriteLine("longest substring :" +test);   
     }
     public string LongestCommonPrefix(string[] strs) {
-        return "";
+        int longestMatch = 0;
+        bool lengthOver200 = strs.Where(a => a.Length > 200).Any();
+        
+        for(int i = 0; i < strs.Length; i++){
+            for(int j = i + 1; j < strs.Length; j++){
+                int currentMatch = GetLongestMatchNumberBetweenTwoString(strs[i],strs[i+1]);
+                longestMatch = currentMatch;         
+            }
+        }
+        
+        return strs[0].Substring(0,longestMatch);
+    }
+    public int GetLongestMatchNumberBetweenTwoString(string s1, string s2){
+        int longestMatchBetweenTheseTwo = 0;
+        for(int k = 0; k < s1.Length; k++){
+            char c1 = s1[k];
+            char c2 = s2[k];
+            if(c1 == c2)
+                longestMatchBetweenTheseTwo++;
+        }
+        return longestMatchBetweenTheseTwo;
     }
 }
