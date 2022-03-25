d1807e43e5306f65e4f085632b68ace61f7a128f
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-25 13:47:30 +0200

    initial commit on this branch

--- Program.cs
+++ Program.cs
@@ -2,33 +2,12 @@
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
-        int test = program.RomanToInt("LVIII");
+        string[] testString = {"Array","Of","String"};
+        
+        string test = program.LongestCommonPrefix(testString);
         Console.WriteLine(test);   
     }
-    public  int RomanToInt(string s){
-        var romanAndIntMapping = new Dictionary<char, int>(){
-	        {'I', 1},
-	        {'V', 5},
-	        {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
-        int output = 0;
-        for(int i =  0; i < s.Length; i++)
-        {
-            char currentChar = s.ElementAt(i);
-            char nextChar = i == s.Length-1 ? s.ElementAt(i) : s.ElementAt(i+1);
-             if(!romanAndIntMapping.ContainsKey(currentChar))
-                return 0;
-             else{
-                    if(romanAndIntMapping[currentChar] < romanAndIntMapping[nextChar])
-                        output-= romanAndIntMapping[currentChar];    
-                    else
-                        output+= romanAndIntMapping[currentChar];
-             }
-        }
-        return output;
+    public string LongestCommonPrefix(string[] strs) {
+        return "";
     }
 }
