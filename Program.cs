dce060f5d219b8b28cb8d4643d52d4981aa6212b
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-26 03:26:54 +0200

    First working commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿    // See https://aka.ms/new-console-template for more information
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
         
-        string inputValue = "(){}";
+        string inputValue = "({}){}";
         bool isPalindrome = IsValid(inputValue);
         Console.WriteLine("9_Palindrome:Start "+isPalindrome);   
     }
 
     public static bool IsValid(string s) {
-        return false;
+        char[] openBrac = {'(','{','['};
+        char[] closeBrac = {')','}',']'};
+        for(int i=0,j = i+1;i < s.Length-1; i++,j++){
+            int Openidx = Array.IndexOf(openBrac,s[i]);
+            int Closeidx = Array.IndexOf(closeBrac,s[j]);
+            if(Openidx != Closeidx)
+                return false;
+        }
+        return true;
     }
 
 }
