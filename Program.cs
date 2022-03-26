a8fe1ceb56f6ee579fd4d7a01987f3374096aa8e
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-26 02:26:51 +0200

    accepted palindrome code

--- Program.cs
+++ Program.cs
@@ -3,14 +3,21 @@ public class Program{
     public static void Main(string[] args){
         Program program = new Program();
         
-        int palindrome = 121;
+        int palindrome = 22222222;
+        var test = palindrome.ToString().ToArray();
         bool isPalindrome = IsPalindrome(palindrome);
         Console.WriteLine("9_Palindrome:Start "+isPalindrome);   
     }
 
-    private static bool IsPalindrome(int palindrome)
+    private static bool IsPalindrome(int x)
     {
-        throw new NotImplementedException();
+        int temp = x;
+        int reversedNumber = 0;
+        while(temp > 0){
+            reversedNumber = temp % 10 + reversedNumber * 10;
+            temp = temp /10;
+        }
+        return reversedNumber == x;
     }
 
 }
