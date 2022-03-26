cbc2257bf18e29a90a8506ad6eb77dbbe1fdafdf
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-27 00:35:51 +0200

    first working version

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-﻿    // See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
         
-        string inputValue = "({}){}";
+        string inputValue = "{}()";
         bool isPalindrome = IsValid(inputValue);
-        Console.WriteLine("9_Palindrome:Start "+isPalindrome);   
+        Console.WriteLine("20_Valid parentheses:Start "+isPalindrome);   
     }
-
+//stack:first in last out
+    /*
     public static bool IsValid(string s) {
         char[] openBrac = {'(','{','['};
         char[] closeBrac = {')','}',']'};
@@ -18,6 +20,41 @@ public class Program{
                 return false;
         }
         return true;
+    }
+    */
+    public static bool IsValid(string s){
+        Dictionary<char,char> bracMaps= new Dictionary<char, char>(){
+            {'(',')'},
+            {'{','}'},
+            {'[',']'}
+        };
+        //create a stack
+        Stack stack = new Stack();
+        for(int i = 0; i < s.Length ; i++){
+           char c = s[i];
+           if(bracMaps.Keys.Contains(c)){
+               //if its an open bracket then we push into stack, otherwise pop
+                stack.Push(c);
+           }else{
+                if(stack.Count > 0){
+                    //we go the last element of the stack and pop it.
+                    char top = (char)stack.Pop();
+                    //top of stack is '['
+                    //current element is ']'
+                    
+                    // var test1 = s[i];
+                    // var test2 = bracMaps[top]; //pair of current element
+                    //if pair of top element of stack != current element then returning false
+                    if(s[i] != bracMaps[top]){
+                        return false;
+                    }    
+                }else{
+                        return false;
+                    }
+           }
+        }
+        if(stack.Count > 0) return false;
+           else return true;
     }
 
 }
