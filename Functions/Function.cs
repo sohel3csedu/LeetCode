058d2a7596c15f60717d9bb1a596dc5c3380a144
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-28 19:26:59 +0300

    added into functions class

--- Functions/Function.cs
+++ Functions/Function.cs
@@ -11,4 +11,20 @@ public class Functions{
         }
         return output;
     }
+    public int StrStr(string haystack, string needle) {
+        if(needle.Length == 0) return 0;
+        
+        for(int i =0; i < haystack.Length +1 - needle.Length;i++){
+            for(int j = 0; j< needle.Length; j++){
+                if(haystack[i+j] != needle[j]){
+                    break;
+                }
+                if(j == needle.Length -1){
+                    return i;
+                }
+            }
+        }
+        
+        return -1;
+    }
 }
