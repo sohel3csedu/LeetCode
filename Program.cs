337342a013f368280e4e22849a92752bc913dde5
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-28 13:05:35 +0300

    Empty setup for 27

--- Program.cs
+++ Program.cs
@@ -5,40 +5,9 @@ public class Program{
         Program program = new Program();
         int[] nums = {1,1,1,2,2,3,4,5,5,6,6,7};
 
-        Console.WriteLine("26_RemoveDuplicateFromAnSortedArray:Start "+RemoveDuplicates(nums));
+        Console.WriteLine("27_RemoveElement:Start "+RemoveElement(nums,0));
     }
-    public static int RemoveDuplicates(int[] nums) {
-     int left_index = 1; 
-     for(int right_index = 1; right_index < nums.Length; right_index++){
-         if(nums[right_index] != nums[right_index-1]){
-             nums[left_index] = nums[right_index]; //updating the values in array
-             left_index++; //shifting the left pointer
-         }
-     }
-              
-     return left_index;   
+    public static int RemoveElement(int[] nums, int val) {
+        return 0;
     }
-//   public static int RemoveDuplicates(int[] nums) {
-//      int i = 0; 
-//      int j = 1;
-            
-//         while(j <= nums.Length - 1)
-//         {
-//                 if(nums[i] == nums[j])
-//                 j++; //going to next element to check.
-//             else
-//                 nums[++i] = nums[j++];
-//         }    
-//      return ++i;   
-//     }
-    /*
-    //Realizing differnt between ++i and i++ ;pre and post increment
-    public static (int,int,int) testRetun(){
-        int i = 4;
-        int j = i++;
-        int k = ++i;
-        
-        return (i,j,k);
-    }
-    */
 }
