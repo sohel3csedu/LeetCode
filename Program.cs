fb9078882af77df4b4b1cf46d8ea9298d25b791d
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-28 14:24:54 +0300

    initial comment for strstr function

--- Program.cs
+++ Program.cs
@@ -5,30 +5,10 @@ public class Program{
         Program program = new Program();
         int[] nums = {3,2,2,3};
 
-        Console.WriteLine("27_RemoveElement:Start "+RemoveElement(nums,1));
+        Console.WriteLine("28_ImplementStrStr():Start "+StrStr("",""));
     }
-    public static int RemoveElement(int[] nums, int val) {
-        
-        int output = 0;
-        for( int i =0; i < nums.Length ; i++ ){
-            if(nums[i] != val){
-                nums[output] = nums[i];
-                output++;
-            }
-        }
-        return output;
+   public static int StrStr(string haystack, string needle) {
+        return 0;
     }
-    /*
-    //returning correct result here but don't know why it's causing and error on leetcode compiler.
-    //I was not modifying the array elemments that's why it was returning error, it's little bit confusing, because
-    //it said we need to return only the count but still it checks the array elements.
-    public static int RemoveElement(int[] nums, int val) {
-        int output = nums.Count();
-        for(int i =0;i < nums.Count();i++){
-           if(nums[i] == val)     
-                output--;
-        }
-        return output;
-    }
-    */
+
 }
