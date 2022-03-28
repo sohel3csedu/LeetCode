25144aa8f0003bb04434100faf2bbfff4e5aceae
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-28 19:24:03 +0300

    accepted version

--- Program.cs
+++ Program.cs
@@ -4,28 +4,23 @@ public class Program{
     public static void Main(string[] args){
         Program program = new Program();
         
-        Console.WriteLine("28_ImplementStrStr():Start "+StrStr("healo","al"));
+        Console.WriteLine("28_ImplementStrStr():Start "+StrStr("","al"));
     }
    public static int StrStr(string haystack, string needle) {
-        int needleIdx = 0;
         if(needle.Length == 0) return 0;
-        while(needleIdx < needle.Length){
-            for(int i = 0; i < haystack.Length; i++){
-                if(needle[needleIdx] == haystack[i]){
-                    
+        
+        for(int i =0; i < haystack.Length +1 - needle.Length;i++){
+            for(int j = 0; j< needle.Length; j++){
+                if(haystack[i+j] != needle[j]){
+                    break;
+                }
+                if(j == needle.Length -1){
+                    return i;
                 }
             }
         }
-        for(int i = 0; i < needle.Length ; i++){
-            for(int j = 0; j < haystack.Length; j++){
-                if(needle[i] == haystack[j]){
-                    needleIdx = j;
-                }else{
-                    needleIdx = 0;
-                }
-            }    
-        }
-        return needleIdx;
+        
+        return -1;
     }
 
 }
