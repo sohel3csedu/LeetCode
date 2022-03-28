a8504b934878122ed38c727326ba53dbb9fdbfd3
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-28 14:59:42 +0300

    some uncompleted code

--- Program.cs
+++ Program.cs
@@ -3,12 +3,29 @@ using System.Collections;
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
-        int[] nums = {3,2,2,3};
-
-        Console.WriteLine("28_ImplementStrStr():Start "+StrStr("",""));
+        
+        Console.WriteLine("28_ImplementStrStr():Start "+StrStr("healo","al"));
     }
    public static int StrStr(string haystack, string needle) {
-        return 0;
+        int needleIdx = 0;
+        if(needle.Length == 0) return 0;
+        while(needleIdx < needle.Length){
+            for(int i = 0; i < haystack.Length; i++){
+                if(needle[needleIdx] == haystack[i]){
+                    
+                }
+            }
+        }
+        for(int i = 0; i < needle.Length ; i++){
+            for(int j = 0; j < haystack.Length; j++){
+                if(needle[i] == haystack[j]){
+                    needleIdx = j;
+                }else{
+                    needleIdx = 0;
+                }
+            }    
+        }
+        return needleIdx;
     }
 
 }
