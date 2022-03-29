1c35a7100b43c5d0653a95bcb64940bb8f1087da
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-29 11:15:19 +0300

    accepted version

--- Program.cs
+++ Program.cs
@@ -3,24 +3,44 @@ using System.Collections;
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
+        int[] nums = {1,3,5,6};
+        int target = 7;
+
+        // int[] nums = {1,3,5,6,8,10,12};
+        // int target = 15;
         
-        Console.WriteLine("28_ImplementStrStr():Start "+StrStr("halal","al"));
+        // int[] nums = {1,3,5,6};
+        // int target = 5;
+
+        Console.WriteLine("35_Implement_SearchInsert():Start "+SearchInsert(nums,target));
     }
-   public static int StrStr(string haystack, string needle) {
-        if(needle.Length == 0) return 0;
-        
-        for(int i =0; i < haystack.Length +1 - needle.Length;i++){
-            for(int j = 0; j< needle.Length; j++){
-                if(haystack[i+j] != needle[j]){
-                    break;
+   public static int SearchInsert(int[] nums, int target) {
+       
+       int position = binarySearchRecursive(nums,target,0,nums.Length -1);
+        if(position == -1){
+            for(int i = 0; i < nums.Length; i++){
+                if(target > nums[nums.Length -1]){
+                    return nums.Length;
                 }
-                if(j == needle.Length -1){
+                else if(target  < nums[i]){
                     return i;
                 }
-            }
         }
-        
-        return -1;
+       }
+       
+       return position;
     }
-
+    public static int binarySearchRecursive(int[]nums, int target,int left,int right ){
+        if(left > right){
+            return -1;
+        }
+        int midPoint = (left + right) / 2;
+        if(nums[midPoint] == target){
+            return midPoint;
+        }else if(target < nums[midPoint]){
+            return binarySearchRecursive(nums,target,left,midPoint-1);
+        }else{
+            return binarySearchRecursive(nums,target,midPoint+1,right);
+        }
+    }
 }
