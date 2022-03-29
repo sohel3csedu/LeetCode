da7bc974035fa00f52b92e30744a5198c57f1932
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-29 11:21:10 +0300

    added to functions file

--- Functions/Function.cs
+++ Functions/Function.cs
@@ -27,4 +27,33 @@ public class Functions{
         
         return -1;
     }
+    public static int SearchInsert(int[] nums, int target) {
+       
+       int position = binarySearchRecursive(nums,target,0,nums.Length -1);
+        if(position == -1){
+            for(int i = 0; i < nums.Length; i++){
+                if(target > nums[nums.Length -1]){
+                    return nums.Length;
+                }
+                else if(target  < nums[i]){
+                    return i;
+                }
+        }
+       }
+       
+       return position;
+    }
+    public static int binarySearchRecursive(int[]nums, int target,int left,int right ){
+        if(left > right){
+            return -1;
+        }
+        int midPoint = (left + right) / 2;
+        if(nums[midPoint] == target){
+            return midPoint;
+        }else if(target < nums[midPoint]){
+            return binarySearchRecursive(nums,target,left,midPoint-1);
+        }else{
+            return binarySearchRecursive(nums,target,midPoint+1,right);
+        }
+    }
 }
