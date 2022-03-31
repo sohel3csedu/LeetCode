1b1e93d381bf9c66e1613b7a4e736bb08bb42f8f
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-03-31 16:59:34 +0300

    accepted version

--- LengthOfLastWord.cs
+++ LengthOfLastWord.cs
@@ -2,4 +2,13 @@ public static class LengthOfLastWord{
     public static void test(){
         Console.WriteLine("Console");
     }
+    public static int lengthOfLastWord(string s) {
+        int output=0;
+        s = s.Trim();
+        for(int i=s.Length-1;i>=0;i--){
+            if(char.IsWhiteSpace(s[i])) break;
+            else output++;
+        }
+        return output;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,11 +3,14 @@ using System.Collections;
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
-        LengthOfLastWord.test();
+
+        //string _string = "   fly me   to   the moon  ";
+        string _string = "luffy is still joyboy";
+        int output = LengthOfLastWord.lengthOfLastWord(_string.Trim());
         
         int[] nums = {5,4,-1,7,8};
 
-        Console.WriteLine("58_length_of_last_world:Start ");
+        Console.WriteLine("58_length_of_last_world:Start "+output);
     }
     
     
