9d3daaa4fae219f79baa2caddd5d3814e8bc0965
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-05 12:59:33 +0300

    accepted version

--- ContainsDuplicate.cs
+++ ContainsDuplicate.cs
@@ -1,5 +1,12 @@
 public class ContainsDuplicate{
-    public bool containsDuplicate(){
+    public bool containsDuplicate(int[] nums){
+        List<int> distinctNumbers = new List<int>();
+        for(int i = 0;i < nums.Length;i++){
+            if(distinctNumbers.Contains(nums[i]))
+                return true;
+            else
+                distinctNumbers.Add(nums[i]);
+        }
         return false;
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,10 +3,10 @@ using System.Collections;
 public class Program{
     public static void Main(string[] args){
         Program program = new Program();
-        
-        Add_Binary add_Binary = new Add_Binary();
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
         ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-        var output = containsDuplicate.containsDuplicate();
+        var output = containsDuplicate.containsDuplicate(nums);
+        Console.WriteLine(output);
     }    
 
 }
