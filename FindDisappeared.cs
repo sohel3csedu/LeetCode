1f26be102e3f768afb3209f8f2d353b90d0ee5ba
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-06 13:33:08 +0300

    accepted version yeaa

--- FindDisappeared.cs
+++ FindDisappeared.cs
@@ -1,10 +1,14 @@
 public class FindDisappeared{
     public IList<int> FindDisappearedNumbers(int[] nums) {
         IList<int> output = new List<int>();
-        for(int i = 1;i <= nums.Count();i++){
-            if(!nums.Contains(i)){
-                output.Add(i);
-            }
+        for(int i = 0;i < nums.Count();i++){
+            int newIdx = Math.Abs(nums[i])-1;
+            int newValue = -1*Math.Abs(nums[newIdx]);   //updating the existing array with - only, not changing the values there.
+            nums[newIdx] = newValue;
+        }
+        for(int i = 0;i < nums.Length;i++){
+            if(nums[i] > 0)
+                output.Add(i+1);
         }
         return output;
     }
