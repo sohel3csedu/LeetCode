0271949db73a4b160c401893e432b0200c360c4e
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-06 10:32:22 +0300

    initial commit

---
+++ FindDisappeared.cs
@@ -0,0 +1,7 @@
+public class FindDisappeared{
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        IList<int> output = new List<int>();
+        
+        return output;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ public class Program{
     public static void Main(string[] args){
         Program program = new Program();
         int[] nums = {9,6,4,2,3,5,7,0,8};
-        MissingNumber missingNumber = new MissingNumber();
-        var output = missingNumber.getMissingNumber(nums);
+        FindDisappeared _findDisappeared = new FindDisappeared();
+        var output = _findDisappeared.FindDisappearedNumbers(nums);
         Console.WriteLine(output);
     }    
 
