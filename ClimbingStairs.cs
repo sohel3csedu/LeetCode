16774dc47ae363fb1ee116d340dc14df92b4093d
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-08 13:37:15 +0300

    with some bottom up apporach

--- ClimbingStairs.cs
+++ ClimbingStairs.cs
@@ -12,9 +12,22 @@ public class ClimbingStairs{
     }
     */
     //solving recursive way
+    /*
     public int ClimbStairs(int n) {
         if(n == 0 || n == 1) return 1;
         else
             return ClimbStairs(n-1)+ClimbStairs(n-2);
+    }
+    */
+    //some bottom up solution
+    public int ClimbStairs(int n) {
+        int[] nums = new int[n+1];
+        if(n == 1 || n == 0) return 0;
+        nums[0] = 1;
+        nums[1] = 1;
+        for(int i = 2;i<=n;i++){
+            nums[i] = nums[i-1]+nums[i-2];
+        }
+        return nums[n];
     }
 }
