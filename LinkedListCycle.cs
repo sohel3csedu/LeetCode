c0e8bb76e3d832a09585b114d502241b157b644c
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-14 12:12:51 +0300

    solution using hashset

--- LinkedListCycle.cs
+++ LinkedListCycle.cs
@@ -1,9 +1,16 @@
 public class LinkedListCycle{
     public bool HasCycle(ListNode head) {
+        HashSet<ListNode> listNodes = new HashSet<ListNode>();
+        
+        while(head.next != null){
+            if(listNodes.Contains(head))
+                return true;
+            listNodes.Add(head);
+            head = head.next;
+        }
         return false;   
     }
 }
-
   public class ListNode {
       public int val;
       public ListNode next;
