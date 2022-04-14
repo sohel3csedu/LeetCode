181f7de71e2c938c281f41618f1c615c1833ec07
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-14 14:26:38 +0300

    intial setup

---
+++ PalindromeLinkedList.cs
@@ -0,0 +1,14 @@
+public class PalindromeLinkedList{
+    public bool IsPalindrome(ListNode head) {
+        
+        return false;   
+    }
+}
+public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
