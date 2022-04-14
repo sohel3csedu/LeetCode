404b4250da18f033f5256e09f84c49cb5d54bf61
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-14 12:28:59 +0300

    using two pointer traversing, but the runtime is same

--- LinkedListCycle.cs
+++ LinkedListCycle.cs
@@ -1,4 +1,5 @@
 public class LinkedListCycle{
+    /*
     public bool HasCycle(ListNode head) {
         if(head == null) return false;
         HashSet<ListNode> listNodes = new HashSet<ListNode>();
@@ -10,6 +11,18 @@ public class LinkedListCycle{
             head = head.next;
         }
         return false;   
+    }
+    */
+    public bool HasCycle(ListNode head){
+        ListNode fastpointer = head;
+        ListNode slowpointer = head;
+        while(fastpointer!= null && fastpointer.next != null){  //why not slow pointer checking? may be fastpointer reaches earlier
+            fastpointer = fastpointer.next.next;
+            slowpointer = slowpointer.next;
+            if(fastpointer == slowpointer)
+                return true;
+        }
+        return false;
     }
 }
   public class ListNode {
