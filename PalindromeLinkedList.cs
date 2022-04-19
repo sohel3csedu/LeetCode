bd15f33de638b1f4908cc9a86ea1889be3346cf3
Mofizur Rahaman <mofizur.rahaman@okperinta.fi>
2022-04-19 14:21:46 +0300

    middle of work

--- PalindromeLinkedList.cs
+++ PalindromeLinkedList.cs
@@ -1,7 +1,24 @@
 public class PalindromeLinkedList{
     public bool IsPalindrome(ListNode head) {
-        
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        while(fastPointer.next != null){
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
         return false;   
+    }
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
     }
 }
 public class ListNode {
